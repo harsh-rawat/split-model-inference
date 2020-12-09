1e401ce163e971be83c28430032d7eee3e28fd1d
Harsh Rawat <harsh.rawat@wisc.edu>
2020-12-09 16:40:29 -0600

    fixes

--- distributed_setup/client.py
+++ distributed_setup/client.py
@@ -14,21 +14,13 @@ def set_client_connection(host, port):
 def get_data(socket_connection, batch_idx, network_latency_timer: Timer):
     print('fetching data')
     data = []
-    i = 0
-    size = 0
     while True:
-        i += 1
         packet = socket_connection.recv(4096)
-        size += len(packet)
-        if packet[-1] == 68 and packet[-2] == 78 and packet[-3] == 69:
-            data.append(packet[0:-4])
+        if not packet:
             break
         data.append(packet)
     network_latency_timer.record(batch_idx)
 
     data_arr = pickle.loads(b"".join(data))
-    print('Sending ack from client for batch {}'.format(batch_idx))
-    ack_text = "This is ACK for batch " + str(batch_idx)
-    socket_connection.send(ack_text.encode())
-
+    print(data_arr)
     return data_arr
--- distributed_setup/server.py
+++ distributed_setup/server.py
@@ -14,15 +14,5 @@ def set_server_connection(port):
 def send_data(s, data):
     print('Sending data')
     data = pickle.dumps(data)
-
-    s.send(data)
-    end_tag = "END"
-    print("End: {}".format(end_tag))
-    bytes_sent = s.send(end_tag.encode())
-    print('Will start waiting for ack')
-    # wait until ACK received from node1
-    recv_data = str(s.recv(1024))
-    while not recv_data:
-        recv_data = str(s.recv(1024))
-    print('Acknowledged received: {}'.format(recv_data))
+    bytes_sent = s.send(data)
     return bytes_sent
