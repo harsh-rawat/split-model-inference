9bdf40a87fad4edfc3601fb0769eb11f741a5047
akshatjain029 <ajain85@wisc.edu>
2020-12-09 03:19:15 -0600

    Update client.py
    Adding sync between client server

--- distributed_setup/client.py
+++ distributed_setup/client.py
@@ -8,12 +8,16 @@ def set_client_connection(host, port):
     return server_socket
 
 
-def get_data(socket_connection):
+def get_data(socket_connection, batch_idx):
     data = []
     while True:
         packet = socket_connection.recv(4096)
         if not packet:
             break
         data.append(packet)
+
     data_arr = pickle.loads(b"".join(data))
+    ack_text = "This is ACK for batch " + str(batch_idx)
+    socket_connection.send(ack_text.encode())
+
     return data_arr
