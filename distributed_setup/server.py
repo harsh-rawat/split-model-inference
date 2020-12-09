b4c89f3b6cdac18c89862ac049adeea9aad10d11
akshatjain029 <ajain85@wisc.edu>
2020-12-09 03:17:56 -0600

    Update server.py
    Adding sync

--- distributed_setup/server.py
+++ distributed_setup/server.py
@@ -2,14 +2,22 @@ import pickle
 import socket
 
 
-def set_server_connection(port):
+def set_client_connection(host, port):
     server_socket = socket.socket()
-    server_self_hostname = socket.gethostname()
-    server_socket.bind((server_self_hostname, port))
-    server_socket.listen(5)
+    server_socket.connect((host, port))
     return server_socket
 
 
-def send_data(s, data):
-    data = pickle.dumps(data)
-    s.send(data)
+def get_data(socket_connection, batch_idx):
+    data = []
+    while True:
+        packet = socket_connection.recv(4096)
+        if not packet:
+            break
+        data.append(packet)
+
+    data_arr = pickle.loads(b"".join(data))
+    ack_text = "This is ACK for batch " + str(batch_idx)
+    socket_connection.send(ack_text.encode())
+
+    return data_arr
